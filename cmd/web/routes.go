a6863c1b4ba4dc4d912e4a2f48216fdfe5947424
gosmartwizard <gosmartwizard@gmail.com>
2023-08-12 18:12:45 -0400

    Moved routes into file to make main short and simple

---
+++ cmd/web/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+
+	mux := http.NewServeMux()
+
+	fileserver := http.FileServer(http.Dir(app.staticDir))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileserver))
+
+	mux.HandleFunc("/", app.HomeHandler)
+
+	mux.HandleFunc("/quint/create", app.QuintCreateHandler)
+	mux.HandleFunc("/quint/update", app.QuintUpdateHandler)
+	mux.HandleFunc("/quint/delete", app.QuintDeleteHandler)
+	mux.HandleFunc("/quint/list", app.QuintListHandler)
+	mux.HandleFunc("/quint/get", app.QuintGetHandler)
+
+	return mux
+}
