ae7e734d461ec3ec180421e368e1c5a4f3f0b37c
gosmartwizard <gosmartwizard@gmail.com>
2023-08-13 15:48:12 -0400

    Added exec sql stmt to create quints into DB

--- internal/models/quints.go
+++ internal/models/quints.go
@@ -18,7 +18,21 @@ type QuintModel struct {
 }
 
 func (m *QuintModel) Insert(title string, content string, expires int) (int, error) {
-	return 0, nil
+
+	stmt := `INSERT INTO quints (title, content, created, expires)
+			VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), err
 }
 
 func (m *QuintModel) Get(id int) (*Quint, error) {
